1f82a22f007b802243b1f9c738503af821520f2b
mhdiirsyad <irsyadcahkalipucang@gmail.com>
2025-09-22 18:04:13 +0700

    feat: login, me, register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterStoreRequest;
 use App\Http\Resources\UserResource;
+use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -44,6 +48,54 @@ class AuthController extends Controller
                 'message' => 'User Profile finded',
                 'data'=> new UserResource($user),
             ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Errors occurred',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function logout() {
+        try {
+            $user = Auth::user();
+            $user->currentAccessToken()->delete();
+            
+            return response()->json([
+                'message' => 'Logout success',
+                'data' => null,
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Errors occurred',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function register(RegisterStoreRequest $request) {
+        $data = $request->validated();
+
+        DB::beginTransaction();
+
+        try {
+            $user = new User();
+            $user['name'] = $data['name'];
+            $user['email'] = $data['email'];
+            $user['password'] = Hash::make(($data['password']));
+            $user->save();
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            DB::commit();
+
+            return response()->json([
+                'message' => 'Register success',
+                'data' => [
+                    'token' => $token,
+                    'user' => new UserResource($user),
+                ],
+            ], 201);
         } catch (Exception $e) {
             return response()->json([
                 'message' => 'Errors occurred',
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
