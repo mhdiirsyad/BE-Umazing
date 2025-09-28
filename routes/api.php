b1ee1dc8244c09a6a5884116283936f28087cb17
mhdiirsyad <irsyadcahkalipucang@gmail.com>
2025-09-28 11:06:31 +0700

    fix: authorization token

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,19 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\BearerMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::middleware('auth:sanctum')->group(function() {
+Route::middleware(['auth:sanctum'])->group(function() {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+Route::middleware(['auth:sanctum', 'admin'])->group(function() {
+    Route::apiResource('/products', \App\Http\Controllers\ProductController::class);
+    Route::apiResource('/categories', \App\Http\Controllers\CategoryController::class);
+    Route::apiResource('/orders', \App\Http\Controllers\OrderController::class)->only(['index', 'show', 'destroy']);
 });
